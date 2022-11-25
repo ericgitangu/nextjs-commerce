18b87c139f972e6b26f180db8efa2b151115d021
Eric <Eric.Gitangu@rgare.com>
2022-11-25 04:36:12 -0600

    Logo update.

--- site/components/ui/Logo/Logo.tsx
+++ site/components/ui/Logo/Logo.tsx
@@ -1,12 +1,7 @@
 import Image from 'next/image'
 
 const Logo = ({ className = '', ...props }) => (
-  <Image
-    src="/icon-512X512.png"
-    alt="Park & Pick Logo"
-    width="32"
-    height="32"
-  ></Image>
+  <Image src="/icon.png" alt="Park & Pick Logo" width="32" height="32"></Image>
 )
 
 export default Logo
